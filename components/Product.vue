bd7d8485f14b9af4afebb7559085e18ff29db559
Jodie <meishanfu@outlook.com>
2021-05-22 20:40:06 +0800

    使用NuxtJS建構vue

---
+++ components/Product.vue
@@ -0,0 +1,52 @@
+<template>
+    <div>
+  <b-card
+    title="Card Title"
+    v-bind:img-src="imgsrc"
+    img-alt="Image"
+    img-top
+    tag="article"
+    style="max-width: 20rem;"
+    class="mb-2"
+  >
+    <b-card-text>
+      {{ text }}
+      {{ imgsrc }}
+    </b-card-text>
+
+    <b-button href="#" variant="primary">放入購物車</b-button>
+  </b-card>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Product',
+  props: {
+    msg: String,
+    text: String,
+    imgsrc : String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
+
+<!-- 註：v-bind:class="{'class 名稱' : vue 屬性名稱}"參考書p.4-28，指定被綁定時的處理動作-->
+<!-- 註：props目的在傳遞資料值 用陣列取值 參考書p.4-39-->
---
+++ layouts/default.vue
@@ -0,0 +1,89 @@
+<template>
+  <div>
+    
+  <b-navbar type="dark" variant="dark">
+    <b-navbar-nav>
+      <b-nav-item href="#">
+        <NuxtLink to="/">Home page
+        </NuxtLink>
+        </b-nav-item>
+      <b-nav-item href="#">
+        <NuxtLink to="/about">About
+        </NuxtLink>
+        </b-nav-item>
+
+      <!-- Navbar dropdowns -->
+      <b-nav-item-dropdown text="Lang" right>
+        <b-dropdown-item href="#">EN</b-dropdown-item>
+        <b-dropdown-item href="#">ES</b-dropdown-item>
+        <b-dropdown-item href="#">RU</b-dropdown-item>
+        <b-dropdown-item href="#">FA</b-dropdown-item>
+      </b-nav-item-dropdown>
+
+      <b-nav-item-dropdown text="User" right>
+        <b-dropdown-item href="#">Account</b-dropdown-item>
+        <b-dropdown-item href="#">Settings</b-dropdown-item>
+      </b-nav-item-dropdown>
+    </b-navbar-nav>
+  </b-navbar>
+  
+    <Nuxt />
+  </div>
+</template>
+
+<style>
+html {
+  font-family:
+    'Source Sans Pro',
+    -apple-system,
+    BlinkMacSystemFont,gz
+    'Segoe UI',
+    Roboto,
+    'Helvetica Neue',
+    Arial,
+    sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+  margin: 0;
+}
+
+.button--green {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #3b8070;
+  color: #3b8070;
+  text-decoration: none;
+  padding: 10px 30px;
+}
+
+.button--green:hover {
+  color: #fff;
+  background-color: #3b8070;
+}
+
+.button--grey {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #35495e;
+  color: #35495e;
+  text-decoration: none;
+  padding: 10px 30px;
+  margin-left: 15px;
+}
+
+.button--grey:hover {
+  color: #fff;
+  background-color: #35495e;
+}
+</style>
